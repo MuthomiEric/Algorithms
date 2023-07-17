9785b71acfb5c306492714d02e0ad579bf2efa0f
Eric Muthomi <emuthomi@jambopay.com>
2023-07-17 05:59:35 +0300

    Merge sort algorithm

---
+++ Algorithms/MergeSort/MergeSortAlg.cs
@@ -0,0 +1,87 @@
+﻿namespace Algorithms.MergeSort
+{
+    internal static class MergeSortAlg
+    {
+        public static void MergeSort(int[] array)
+        {
+            int length = array.Length;
+
+            if (length <= 1) return; //base case
+
+            int middle = length / 2;
+
+            int[] leftArray = new int[middle];
+
+            int[] rightArray = new int[length - middle];
+
+            int i = 0; //left array pointer
+
+            int j = 0; //right array pointer
+
+            for (; i < length; i++)
+            {
+                if (i < middle)
+                {
+                    leftArray[i] = array[i];
+                }
+                else
+                {
+                    rightArray[j] = array[i];
+
+                    j++;
+                }
+            }
+            MergeSort(leftArray);
+
+            MergeSort(rightArray);
+
+            Merge(leftArray, rightArray, array);
+        }
+
+        private static void Merge(int[] leftArray, int[] rightArray, int[] array)
+        {
+
+            int leftSize = array.Length / 2;
+
+            int rightSize = array.Length - leftSize;
+
+            int mainArr = 0, left = 0, right = 0; //indices
+
+            while (left < leftSize && right < rightSize)
+            {
+                if (leftArray[left] < rightArray[right])
+                {
+                    array[mainArr] = leftArray[left];
+
+                    mainArr++;
+
+                    left++;
+                }
+                else
+                {
+                    array[mainArr] = rightArray[right];
+
+                    mainArr++;
+
+                    right++;
+                }
+            }
+            while (left < leftSize)
+            {
+                array[mainArr] = leftArray[left];
+
+                mainArr++;
+
+                left++;
+            }
+            while (right < rightSize)
+            {
+                array[mainArr] = rightArray[right];
+
+                mainArr++;
+
+                right++;
+            }
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,23 @@
+﻿// Time complexity = O(n Log n)
+// Space complexity = O(n)
+
+using Algorithms.MergeSort;
+
+int[] array = { 7, 2, 5, 0, 3, 8, 4, 9, 1, 6 ,10};
+
+Console.WriteLine("Before Sorting");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+MergeSortAlg.MergeSort(array);
+
+Console.WriteLine("");
+Console.WriteLine("After Sorting");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+Console.ReadLine();
